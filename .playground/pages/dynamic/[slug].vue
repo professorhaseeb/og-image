a633dc93da9c8060688a48bf9a7c6de7d1eef214
Haseeb ur Rehman <hello@haseeburrehman.com>
2024-03-30 21:36:46 +0500

    update

---
+++ .playground/pages/dynamic/[slug].vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <pre>
+      {{ $route?.params }}
+    </pre>
+  </div>
+</template>
+
+
+<script setup>
+
+import { useRoute, defineOgImage,defineOgImageComponent, definePageMeta, useServerSeoMeta } from '#imports'
+
+let route = useRoute();
+
+defineOgImageComponent('Nuxt', {
+  headline: route?.params?.slug,
+  title: 'Version 3 🎉',
+  description: 'Nuxt OG Image v3 is here!',
+})
+</script>
